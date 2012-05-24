c00d71751c88eb24739cb8d29588494970395486
Jorge Pardiñas <jparbros@gmail.com>
2012-05-24 11:13:33 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+puts "Leyendo archivo"
+casillas = File.read('doc/casillas_2012.csv').force_encoding('MacRoman').encode('UTF-8')
+
+puts "Parseando archivo"
+casillas_array = CSV.parse(casillas, :headers => true)
+
+casillas_array.each do |casilla|
+  estado = State.find_or_create_by_name(casilla['NOMBRE_ESTADO'])
+
+  puts "Creando casilla #{casilla['CABECERA_DISTRITAL']} #{casilla['SECCION']} #{casilla['TIPO_CASILLA_W']} \n"
+
+  PollingBooth.create( address: casilla['DOMICILIO'],
+    distric: casilla['ID_DISTRITO'],
+    district_head: casilla['CABECERA_DISTRITAL'],
+    location: casilla['UBICACION_CASILLA'],
+    nominal_list: casilla['LISTA_NOMINAL_CASILLA'],
+    references: casilla['REFERENCIA_CASILLA'],
+    section: casilla['SECCION'],
+    state_id: estado.id,
+    type: casilla['TIPO_CASILLA_W']
+  )
+end
